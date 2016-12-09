874571c9c35c4c7c649453f7ed611ff726826f50
Curious <skunshd@gmail.com>
2016-12-09 14:34:11 +0200

    Comment fields added

--- src/components/Page/Post.js
+++ src/components/Page/Post.js
@@ -1,12 +1,18 @@
 import React from 'react'
 
 export default class Post extends React.Component {
+  onBtnClickComment = (e) => {
+    e.preventDefault()
+    console.log(this.inputComment.value)
+    this.inputComment.value = ''
+  }
   render() {
     const { user, text } = this.props.post
     return <div className='post'> 
       <h3>Name: {user}</h3>
       <p>Text:  {text}</p>
-      <button>Add comment</button>
+      <input ref={(input) => this.inputComment = input} />
+      <button onClick={this.onBtnClickComment}>Add comment</button>
     </div>
   }
 }
